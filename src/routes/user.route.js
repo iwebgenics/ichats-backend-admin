56e7fac4166fabbf871c8e900c2b9dd118774858
iwebgenics <developer@iwebgenics.com>
2025-03-17 14:42:27 +0530

    Updated backend admin code

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllUsersByRole } from "../controllers/user.controller.js";
+import { getAllUsersByRole,deleteUser } from "../controllers/user.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
